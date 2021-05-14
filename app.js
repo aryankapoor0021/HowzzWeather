03993ccc8a06644e3f42686a874defe860b4f6b7
Aryan Kapoor <aryankapoorjagadhri@gmail.com>
2021-05-15 00:05:49 +0530

    Version 2

--- app.js
+++ app.js
@@ -19,7 +19,19 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get("/",function(req,res){
- res.sendFile(__dirname+"/index.html");
+
+  var today=new Date();
+  
+  var options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  }
+  var day=today.toLocaleDateString("en-IN",options);
+
+  
+
+  res.render("index",{date:day});
 })
 
 app.post("/Result",function(req,res) {
@@ -28,6 +40,22 @@ app.post("/Result",function(req,res) {
   const city=req.body.cityName;
   const unit="metric";
   const apiId=config.apikey;
+
+  var today=new Date();
+  
+  var time="";
+  setInterval(
+  () => time=new Date().toLocaleTimeString(),
+  1000
+   );
+  console.log(time);
+  var options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  }
+  var day=today.toLocaleDateString("en-IN",options);
+
   var url="https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid=" +apiId + "&units="+unit;
 
   https.get(url,function(response){
@@ -35,12 +63,20 @@ app.post("/Result",function(req,res) {
   	response.on("data",function(data){
   		const weatherData=JSON.parse(data);
   		const temprature=weatherData.main.temp;
-  		const weatherDescription=weatherData.weather[0].description;
+  		var weatherDescription=weatherData.weather[0].description;
   		const icon=weatherData.weather[0].icon;
+      const mintemp=weatherData.main.temp_min;
+      const maxtemp=weatherData.main.temp_max;
+      const feeltemp=weatherData.main.feels_like;
+      const humidity=weatherData.main.humidity;
+      const windspeed=weatherData.wind.speed;
+      weatherDescription=capitalize(weatherDescription);
+      const winddeg=weatherData.wind.deg;
   		const imgurl="http://openweathermap.org/img/wn/" + icon + "@2x.png";
   		console.log(weatherDescription);
       console.log(temprature);
-      res.render("output",{source:imgurl,city:city,tempR:temprature});
+
+      res.render("output",{time:time,date:day,source:imgurl,city:city,tempR:temprature,mintemp:mintemp,maxtemp:maxtemp,feeltemp:feeltemp,weatherDescription:weatherDescription,humidity:humidity,windspeed:windspeed,winddeg:winddeg});
   	});
   });
   
@@ -49,7 +85,21 @@ app.post("/Result",function(req,res) {
  });
 
 });
-
+// function clock(){
+//   var today=new Date();
+//   var hours=today.getHours()>10?today.getHours():"0"+today.getHours();
+//   var minutes=today.getMinutes()>10?today.getMinutes():"0"+today.getMinutes();
+//   var seconds=today.getSeconds()>10?today.getSeconds():"0"+today.getSeconds();
+//   var time = hours + ":" + minutes + ":" + seconds;
+// }
+function capitalize(input) {  
+    var words = input.split(' ');  
+    var CapitalizedWords = [];  
+    words.forEach(element => {  
+        CapitalizedWords.push(element[0].toUpperCase() + element.slice(1, element.length));  
+    });  
+    return CapitalizedWords.join(' ');  
+}  
 app.listen(port,function(){
 	console.log("Server Started at 3000");
 });
